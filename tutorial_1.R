97099a6fb05fc736d5545f0049745cba65541575
Tom Franklin <tom.franklin95@hotmail.co.uk>
2019-01-30 23:10:30 +0000

    Course dev

--- tutorial_1.R
+++ tutorial_1.R
@@ -7,27 +7,30 @@
 # 1. Install and load libraries
 # 2. Load data 
 # 3. Data manipulation with dplyr
-# 4. Graphics with ggplot2 
+# 4. Data summarisation 
+# 5. Graphics with ggplot2 
 
 ####
 # 1. Install and load libraries ----
 
-# Installing R package
+# Installing R packages
 install.packages("dplyr")
 
-# How would I install the ggplot2 package? 
-
-# Loading R package
+# Loading R packages
 library("dplyr")
 
+# Tasks: 
+# How would I install the ggplot2 package? 
 # How would I load the ggplot2 package? 
+# Do the same for following packages:
+# `skimr`, `RColorBrewer` and `ggthemes` 
 
 ####
 # 2. Load data ----
-passenger_data <- utils::read.csv("data/passenger_data.csv")
+raw_data <- utils::read.csv("data/passenger_data.csv")
 
 # This is equivilant but we don't use this! 
-utils::read.csv("data/passenger_data.csv") -> passenger_data 
+utils::read.csv("data/passenger_data.csv") -> raw_data 
 
 ####
 # 3. Data manipulation with dplyr ----
@@ -36,16 +39,72 @@ utils::read.csv("data/passenger_data.csv") -> passenger_data
 
 # Selecting columns 
 
-passenger_data %>%
+raw_data %>%
   dplyr::select()
 
+# Task:
+
+# i. Select Age and Sex columns only 
+
+# ii. Select all data apart from the Survived column
+
+# iii. Select the first three variables using numeric 
+
+# Filtering data
+
+# i. Filter data to keep only those where Pclass (passenger class) is equal to 1
+
+# 11. Filter the data to keep only data where there's first 
+#     class passengers and passengers are aged over 50
+
+# iii. Filter data to keep only 2nd and 3rd Class passengers 
+
+# iv. Filter data for only those who Embarked n the journey from Cherbourg
+
+# Renaming data
+
+# i. Rename the Sex column to be Gender, 
+
+# tip: rename(new_column_name = old_column_name)
+
+# Arranging data
+
+# i. Arrange the dataframe from low to high 
+
+# ii. Arrange Fare data from high to low
+
+# Make new variablea (mutate)
+
+# i. Create a new variable called fare_in_dollars, multiplying the fare by a conversion rate of 1.37
+
+# ii. Create an estimate of a passengers birth year by using their Age information!
+
+# iii. Create a flag to indicate those who have an above average income (£29.70)
+
+# Summary statistics - refer to the booklet for tasks! 
+
+# Recoding data 
+
+# i. Recode the integer values of passenger classes to be "1st Class", "2nd Class" etc...
+
+# Distinct data
+
+# i. Get a distinct list of the Cabin names on the titanic 
+
+# Advanced exercises 
+
+
+####
+# 4. Data summarisation (using base R syntax)
+
+table(raw_data$Sex)
 
 
 
 
 ####
-# 4. Graphics with ggplot2 ----
-passenger_data %>%
+# 5. Graphics with ggplot2 ----
+raw_data %>%
   ggplot() + 
   geom_bar(aes(x = Pclass))
 
